d5f089478ce897f555046c7efc2d66e9ddf74b89
paullaster <paullasterokoth98@gmail.com>
2022-10-11 15:56:51 +0300

    adding test

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ const Router = express.Router();
 /**
  * Internal module dependencies
  */
-const homePage = require ( '../controllers/home');
 const signUpPage = require ( '../controllers/sign_up');
 const loginPage = require ( '../controllers/login');
 
@@ -15,7 +14,6 @@ const loginPage = require ( '../controllers/login');
 /**
  * Routes 
  */
-Router.get ('/', homePage);
 Router.post ( '/signup', signUpPage);
 Router.post ( '/login', loginPage);
 
