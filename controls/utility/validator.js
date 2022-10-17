16d20ca7529524fcef7622d73b2154630c76be49
paullaster <paullasterokoth98@gmail.com>
2022-10-17 15:27:28 +0300

    satizing user input!

--- controls/utility/validator.js
+++ controls/utility/validator.js
@@ -7,6 +7,31 @@
 const validator = require ( 'validator' );
 
 //Validation function
-const validate = (req,res,next) => {};
+const validate = (req, res, next) => {
+    const schema = {
+        [req.body.email]: {
+            validate: [validator.isEmail (), "This is not a valid email address"],
+            trim: [validator.trim ()],
+            escape: [validator.escape ()],
+        },
+        [req.body.fname]: {
+            trim: [validator.trim ()],
+            escape: [validator.escape ()],
+        },
+        [req.body.lname]: {
+            trim: [validator.trim ()],
+            escape: [validator.escape ()],
+        },
+        [req.body.phone]: {
+            trim: [validator.trim ()],
+            escape: [validator.escape ()],
+            mobilenumber: [validator.isMobilePhone (), "Please enter avalid phonenumber"]
+        },
+        [req.body.course]: {
+            trim: [validator.trim ()],
+            escape: [validator.escape ()],
+        },
+    }
+};
 //Export validation function
 module.exports = validate;
