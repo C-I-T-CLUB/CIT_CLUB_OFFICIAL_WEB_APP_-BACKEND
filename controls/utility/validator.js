9e26cc0f596e2fee02192e5f2e148106ec160e4f
paullaster <paullasterokoth98@gmail.com>
2022-10-17 15:01:32 +0300

    anatomy: validate func.

---
+++ controls/utility/validator.js
@@ -0,0 +1,12 @@
+/**
+ * Validation utility function:
+ */
+/**
+ *  Application module dependencies
+ */
+const validator = require ( 'validator' );
+
+//Validation function
+const validate = (req,res,next) => {};
+//Export validation function
+module.exports = validate;
