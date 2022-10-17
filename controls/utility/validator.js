4a4e23bbb0ea8203ac84578fce737c1eb72feb4b
paullaster <paullasterokoth98@gmail.com>
2022-10-17 18:23:33 +0300

    switching express validator

--- controls/utility/validator.js
+++ controls/utility/validator.js
@@ -5,64 +5,45 @@
  *  Application module dependencies
  */
 const validator = require ( 'validator' );
+const { body, validationResult} = require ( 'express-validator');
 
 //Validation function
 const validate = (req, res, next) => {
-    const schema = {
-        [req.body.email]: {
-            validate: [validator.isEmail (req.body.email), "This is not a valid email address"],
-            escape: [validator.escape (req.body.email)],
-        },
-        [req.body.fname]: {
-            escape: [validator.escape (req.body.fname)],
-        },
-        [req.body.lname]: {
-            escape: [validator.escape (req.body.lname)],
-        },
-        [req.body.phone]: {
-            escape: [validator.escape (req.body.phone)],
-            mobilenumber: [validator.isMobilePhone (req.body.phone, 'any'), "Please enter avalid phonenumber"]
-        },
-        [req.body.course]: {
-            escape: [validator.escape (req.body.course)],
-        },
-        [req.body.rating]: {
-            escape: [validator.escape (req.body.rating)],
-        },
-        [req.body.designRating]: {
-            escape: [validator.escape (req.body.designRating)],
-        },
-        [req.body.interest]: {
-            escape: [validator.escape (req.body.interest)],
-        },
-        [req.body.password]: {
-            escape: [validator.escape (req.body.password)],
-            /**
-             * const strongPassword = { minLength: 8, 
-             * minLowercase: 1, 
-             * minUppercase: 1, 
-             * minNumbers: 1, 
-             * minSymbols: 1, 
-             * returnScore: false, 
-             * pointsPerUnique: 1, 
-             * pointsPerRepeat: 0.5, 
-             * pointsForContainingLower: 10, 
-             * pointsForContainingUpper: 10, 
-             * pointsForContainingNumber: 10, 
-             * pointsForContainingSymbol: 10 }
-             */
-            strongpassword: [validator.isStrongPassword (req.body.password)],
-        },
-        [req.body.other]: {
-            escape: [validator.escape (req.body.other)],
-        },
-    }
-    res
-    .status (500)
-    .json ({
-        error: schema
-    });
-    next ();
+    //Validating email address
+    body ( 'email')
+    .isEmail ()
+    .normalizeEmail ().toLowerCase ()
+    .trim ()
+    .isEmpty ()
+    .escape ()
+    .withMessage ('Invalid email address provided ');
+
+   //Validating  first name and last name
+   body ('fname')
+   .isAlpha ()
+   .trim ()
+   .isEmpty ()
+   .escape ()
+   .isUppercase ().toLowerCase ()
+   .withMessage ('Invalid first name provided ');
+
+   body ('lname')
+   .isAlpha ()
+   .trim ()
+   .isEmpty ()
+   .escape ()
+   .isUppercase ().toLowerCase ()
+   .withMessage ('Invalid last name provided ');
+
+   //Validating phone number
+   body ('phone')
+   .trim ()
+   .isEmpty ()
+   .isMobilePhone ('any')
+   .escape ()
+   .withMessage ('Invalid first name and phone number provided');
+
+        
 };
 //Export validation function
 module.exports = validate;
