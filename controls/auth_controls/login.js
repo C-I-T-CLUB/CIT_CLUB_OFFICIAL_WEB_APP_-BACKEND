8bf33995d47803f771000893588ee5c1b5dcb12d
paullaster <paullasterokoth98@gmail.com>
2022-10-18 11:15:34 +0300

    streamline members login

--- controls/auth_controls/login.js
+++ controls/auth_controls/login.js
@@ -28,7 +28,6 @@ const login = (req, res, next) => {
                     .json ({
                         id: user._id,
                         email: user.email,
-                        username: user.username,
                         date_joined: user.date,
                         token: newToken,
                     });
@@ -66,7 +65,6 @@ const login = (req, res, next) => {
                                  .json ({
                                      id: user._id,
                                      email: user.email,
-                                     username: user.username,
                                      date_joined: user.date,
                                      token: newToken,
                                  });
@@ -89,52 +87,52 @@ const login = (req, res, next) => {
                      })
                      .catch ( (err) => {
                         CitclubMember.findOne ( { email: email } )
-        .then ( (user) => {
-            bcrypt.compare (password, user.password)
-            .then ( (result) => {
-                if (result) {
-                    //create a new token:
-                    const newToken = token (user);
-                    res
-                    .status (201)
-                    .header ({
-                        'Content-Type': 'application/json',
-                        'auth-token': 'Bearer ' +newToken,
-                    })
-                    .json ({
-                        id: user._id,
-                        email: user.email,
-                        username: user.username,
-                        date_joined: user.date,
-                        token: newToken,
-                    });
-                }else {
-                    res
-                    .status (403)
-                    .json ( {
-                        error: "Incorrect password"
-                    });
-                }
-            })
-            .catch ( (err) => {
-                err.message = "No password provided"
-                res
-                .status (500)
-                .json ({
-                    error: err.message 
-                });
-            });
-        })
-        .catch ( (err) => {
-            err.message = `User with this email or username: '${email}' was not found`;
-            res
-            .status (500)
-            .json ({
-                error: err.message
-            });
-        });
-                     } );
-        } );
+                        .then ( (user) => {
+                            bcrypt.compare (password, user.password)
+                            .then ( (result) => {
+                                if (result) {
+                                    //create a new token:
+                                    const newToken = token (user);
+                                    res
+                                    .status (201)
+                                    .header ({
+                                        'Content-Type': 'application/json',
+                                        'auth-token': 'Bearer ' +newToken,
+                                    })
+                                    .json ({
+                                        id: user._id,
+                                        email: user.email,
+                                        phonenumber: user.phonenumber,
+                                        date_joined: user.date,
+                                        token: newToken,
+                                    });
+                                }else {
+                                    res
+                                    .status (403)
+                                    .json ( {
+                                        error: "Incorrect password"
+                                    });
+                                }
+                            })
+                            .catch ( (err) => {
+                                err.message = "No password provided"
+                                res
+                                .status (500)
+                                .json ({
+                                    error: err.message 
+                                });
+                            });
+                        })
+                        .catch ( (err) => {
+                            err.message = `User with this email or username: '${email}' was not found`;
+                            res
+                            .status (500)
+                            .json ({
+                                error: err.message
+                            });
+                        });
+                                    } );
+                        } );
 };
 
 /**
