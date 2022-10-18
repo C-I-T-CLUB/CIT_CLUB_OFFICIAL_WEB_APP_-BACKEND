f355e4a9d9904d0402a0d51605c6632d4ef01029
paullaster <paullasterokoth98@gmail.com>
2022-10-18 10:46:19 +0300

    add member login func

--- controls/auth_controls/login.js
+++ controls/auth_controls/login.js
@@ -2,19 +2,17 @@
  * Application module dependencies
  */
 const bcrypt = require ( 'bcrypt' );
-const JWT = require ( 'jsonwebtoken');
 /**
  * Module internal dependencies
  */
 const { CitclubMember, CitclubAccount} = require ( '../../database/index');
-const config = require ( '../../config/index');
 const token = require ( '../utility/generateToken');
 /**
  * Login middleware
  */
 const login = (req, res, next) => {
     const {email, password} = req.body;
-    //if (email) {
+    try {
         CitclubAccount.findOne({ email: email })
         .then ( (user) => {
             bcrypt.compare (password, user.password)
@@ -99,6 +97,54 @@ const login = (req, res, next) => {
                          });
                      } );
         } );
+    } catch (error) {
+        CitclubMember.findOne ( { email: email } )
+        .then ( (user) => {
+            bcrypt.compare (password, user.password)
+            .then ( (result) => {
+                if (result) {
+                    //create a new token:
+                    const newToken = token (user);
+                    res
+                    .status (201)
+                    .header ({
+                        'Content-Type': 'application/json',
+                        'auth-token': 'Bearer ' +newToken,
+                    })
+                    .json ({
+                        id: user._id,
+                        email: user.email,
+                        username: user.username,
+                        date_joined: user.date,
+                        token: newToken,
+                    });
+                }else {
+                    res
+                    .status (403)
+                    .json ( {
+                        error: "Incorrect password"
+                    });
+                }
+            })
+            .catch ( (err) => {
+                err.message = "No password provided"
+                res
+                .status (500)
+                .json ({
+                    error: err.message 
+                });
+            });
+        })
+        .catch ( (err) => {
+            err.message = `User with this '${email}' was not found`;
+            res
+            .status (500)
+            .json ({
+                error: err.message
+            });
+        });
+
+    }
 };
 
 /**
