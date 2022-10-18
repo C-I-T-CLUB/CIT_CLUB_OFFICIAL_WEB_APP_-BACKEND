7f827b555bf1bbc26140e7b5401d7e11c6580631
paullaster <paullasterokoth98@gmail.com>
2022-10-18 12:53:03 +0300

    get member details ready!

--- controls/pages/dashboard.js
+++ controls/pages/dashboard.js
@@ -1,18 +1,30 @@
 /**
  * Application module dependencies
  */
+const jwt_decode = require ( 'jwt-decode' );
 
 /**
  * Internal module dependencies
  */
+ const { CitclubMember, CitclubAccount} = require ( '../../database/index');
 
 //Dashboard module
 const dashboard = (req, res ) => {
     const token = req.header ('auth-token').slice ( 7, req.header ('auth-token').length);
-    res
-    .status (201)
-    .json ({
-        body: token
+    const userDetails = jwt_decode (token)
+    CitclubMember.findOne (
+        {_id: userDetails.id}
+    )
+    .then ( (member) => {
+        res
+        .status (200)
+        .json ( {
+            email: member.email,
+            phonenumber: member.phonenumber,
+            programingRating: member.programingRating,
+            designRating: member.designRating,
+            fieldOfInterest: member.fieldOfInterest,
+        });
     });
 };
 
