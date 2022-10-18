2492bfc0d5285366831cfd0d1e84237cc6841ee2
paullaster <paullasterokoth98@gmail.com>
2022-10-18 07:04:49 +0300

    ...

--- controls/utility/generateToken.js
+++ controls/utility/generateToken.js
@@ -3,6 +3,25 @@
  */
 const JWT = require ( 'jsonwebtoken');
 
+/**
+ * Internal module dependencies
+ * @param {*} user 
+ */
+const config = require ( '../../config/index');
 const tokenGenerator = (user) => {
-    
+    const newToken = JWT.sign ( {
+        id: user._id,
+        email: user.email,
+        date_joined: user.date,
+    },
+    config.TOKEN_SECRET,
+    {
+        algorithm: 'HS256',
+        expiresIn: '3600s'
+    }
+    )
+    return newToken;
 };
+
+//Export the token generator
+module.exports = tokenGenerator;
